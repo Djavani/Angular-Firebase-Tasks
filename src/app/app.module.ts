d458edf29b018943e1c9f8f0daa194efeff1d057
djavani <djavanirgomes@gmail.com>
2018-04-24 22:14:00 -0300

    Input Output EventEmiiter - Criando um Presentation Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,38 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { MatToolbarModule } from '@angular/material';
+import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
+import {
+  MatToolbarModule,
+  MatListModule,
+  MatLineModule,
+  MatSlideToggleModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    MatListModule,
+    MatLineModule,
+    MatSlideToggleModule,
     MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor( private afs: AngularFirestore ) {
+    afs.app.firestore().settings({ timestampsInSnapshots: true });
+  }
+ }
